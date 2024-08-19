c0618fb653c227537ba10b9a2c4bf23a362d0aa4
qq <qq@qq.com>
2024-08-20 00:11:37 +0800

    3333

--- build/package-exe.sh
+++ build/package-exe.sh
@@ -137,16 +137,18 @@ https://pan.quark.cn/s/ede840ba6d9d#/list/share          # 我不是药神
 
 # 检查文件是否存在
 if [ -f "Favorite_URL.txt" ]; then
-    current_content=$(cat Favorite_URL.txt)                   # 变量 =读取内容并与预定义内容比较
+	cp Favorite_URL.txt Favorite_URL_BAK.txt
+    current_content=$(cat Favorite_URL_BAK.txt)                   # 变量 =读取内容并与预定义内容比较
     if [ "$current_content" != "$content" ]; then             # 判断 文件是否一致
-        temp_file=$(mktemp)                                   # 变量 =临时文件
-        echo "$content" > "$temp_file"                        # 写入 当前内容写入临时文件中
-        if ! cmp -s "$temp_file" "Favorite_URL.txt"; then     # 判断 仅当内容有真正的差异时才进行写入操作
-            mv "$temp_file" "Favorite_URL.txt"                # 内容不一致，更新文件
-            echo "内容不一致，已更新文件。"
+        # temp_file=$(mktemp)                                   # 变量 =临时文件
+        # echo "$content" > "$temp_file"                        # 写入 当前内容写入临时文件中
+        if ! cmp -s "$temp_file" "Favorite_URL_BAK.txt"; then     # 判断 仅当内容有真正的差异时才进行写入操作
+            mv "$content" "Favorite_URL.txt"                # 内容不一致，更新文件
+			rm Favorite_URL_BAK.txt
+			echo "内容不一致，已更新文件。"
         else
             echo "文件内容一致，无需修改。"
-            rm "$temp_file"                                   # 内容一致，删除临时文件
+            rm Favorite_URL_BAK.txt                                   # 内容一致，删除临时文件
         fi
     else
         echo "文件内容一致，无需修改。"                       # 内容一致，跳过写操作
